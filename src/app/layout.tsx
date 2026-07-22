b070c6f5b32b8a4aa9dcaf23bbf80c6e1c45fc32
Syed-Takmil <syedtakmil@gmail.com>
2026-07-22 21:40:01 +0600

    some changes made in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+
+
+
+
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import { ToastContainer } from "react-toastify";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -9,10 +13,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="flex flex-col min-h-screen bg-slate-50 antialiased text-slate-800">
         
         {/* Navigation Layer */}
-        <Navbar />
+        <Navbar/>
 
         {/* Dynamic Page Workspace Views */}
-        <main className="flex-grow bg-slate-50">
+        <main className="flex-1 bg-slate-50">
           {children}
         </main>
 
